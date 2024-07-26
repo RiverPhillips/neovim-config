b4eec260a3751f780e91a5025acf3ee13ba7bcdd
River Phillips <riverphillips1@gmail.com>
2024-07-26 08:55:08 +0100

    Improve setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,11 +8,14 @@ return {
 		require("mason").setup()
 		require("mason-lspconfig").setup()
 
-		require("lspconfig").lua_ls.setup {}
-		require("lspconfig").rust_analyzer.setup {}
-		require("lspconfig").go_pls.setup {}
-		require("lspconfig").tflint.setup {}
-		require("lspconfig").yamlls.setup {}
+		local lsp = require("lspconfig")
+
+		lsp.lua_ls.setup {}
+		lsp.rust_analyzer.setup {}
+		lsp.gopls.setup {}
+		lsp.tflint.setup {}
+		lsp.yamlls.setup {}
+		lsp.autotools_ls.setup {}
 	end
 
 }
