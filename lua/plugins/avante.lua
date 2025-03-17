38469d1e06b4da0b193e6362ebfd932717fd304d
River Phillips <river.phillips@mettle.co.uk>
2025-03-17 09:26:27 +0000

    some AI improvements

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_set_hl(0, "SolarizedLightCurrentGroup", {
+	fg = "#586E75", -- Solarized Base01 (dark cyan)
+	bg = "#FDF6E3", -- Solarized Base3 (light background)
+	bold = true
+})
+
 return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
@@ -5,11 +11,18 @@ return {
 	opts = {
 		-- add any opts here
 		provider = "copilot",
-		auto_suggestions_provider = "copilot"
+		auto_suggestions_provider = "copilot",
+		highlights = {
+			diff = {
+				current = "SolarizedLightCurrentGroup",
+			}
+		},
+
 	},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"stevearc/dressing.nvim",
+		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		"zbirenbaum/copilot.lua",
@@ -20,6 +33,21 @@ return {
 				file_types = { "markdown", "Avante" },
 			},
 			ft = { "markdown", "Avante" },
+		},
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+				},
+			},
 		},
 	},
 	enabled = true
